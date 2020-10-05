0752901a0bbc08b5ed72f243fccd911bdd84915f
Nabil Hassan <nabil.hassan@clearchannel.co.uk>
2020-10-05 10:08:46 +0100

    Simplify app configuration.

--- src/main/java/net/nh/AppConfig.java
+++ src/main/java/net/nh/AppConfig.java
@@ -4,19 +4,7 @@ import io.dropwizard.Configuration;
 
 public class AppConfig extends Configuration {
 
-    private String appName;
-
-    private String dbConnectionUri;
-
     private String dbName;
-
-    public String getAppName() {
-        return appName;
-    }
-
-    public String getDbConnectionUri() {
-        return dbConnectionUri;
-    }
 
     public String getDbName() {
         return dbName;
